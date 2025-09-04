c7a0bd22e57dd2dd319442159a9ec816771b69d1
Maumita20-09 <maumitas38@gmail.com>
2025-09-04 19:44:58 +0530

    the parking lot's prices system

---
+++ parking_lot.py
@@ -0,0 +1,9 @@
+h=int(input("Enter the hours:"))
+if 1<h<=2:
+    print("Price = 100")
+elif h<=4:
+    print("Price =",100+((h-2)*50))
+elif h<=6:
+    print("Price =",100+100+((h-4)*60))
+else:
+    print("Price =",100+100+100)
